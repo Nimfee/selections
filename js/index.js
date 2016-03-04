75fdee9602619c64b315e5a9a3f3eaabade4f21a
aleks <a.kuznetsova@webinerds.com>
2016-03-04 18:47:02 +0200

    dots to sentence

--- js/index.js
+++ js/index.js
@@ -4,21 +4,31 @@ $(document).ready(function() {
 
 var Sentence = {
     sentences: null,
-    current: null,
     init: function () {
         this.sentences = $('.for_selection');
         this.parse(this.sentences);
         this.dataBoud($('.sentence'));
     },
     parse: function(els) {
-        console.log(Sentence.current);
         els.find('*').each(function () {
-            $(this).html($(this).html().split(/([\.\?!])(?= )/).map(
-                function (v) {
-                    return Sentence.build(v)
-                }
-            ));
+            var parts = $(this).html().split(/([\.\?!])(?= )/);
+
+            $(this).html(Sentence.map(Sentence.addDots(parts)));
         });
+    },
+    addDots: function(parts) {
+        var parts_comma =[];
+        for (var i = 0; i < parts.length; i += 2) {
+            parts_comma.push(parts[i] + (parts[i + 1] ? parts[i + 1] : ''));
+        }
+        return parts_comma;
+    },
+    map: function(parts_comma) {
+        return parts_comma.map(
+            function (v) {
+                return Sentence.build(v)
+            }
+        );
     },
     build: function (v) {
         return '<span class=sentence>' + v + '</span>';
